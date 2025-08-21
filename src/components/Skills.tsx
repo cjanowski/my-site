48c85d79fd29ebb631f2822d8adbf2aa09dfbe6a
Cory J <coryjanowski@gmail.com>
2025-08-20 20:31:04 -0400

    Removed et bubble

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -130,10 +130,6 @@ export default function Skills() {
             <div className="w-3 h-3 rounded-full bg-purple-500"></div>
             <span>Observability</span>
           </div>
-          <div className="flex items-center gap-2">
-            <div className="w-3 h-3 rounded-full bg-orange-500"></div>
-            <span>Emerging Tech</span>
-          </div>
         </motion.div>
       </div>
     </section>
